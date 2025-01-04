cd70f2b8053e7044199d3f6c164094fbebf814a4
jrvscm <chrisjarvisdev@gmail.com>
2025-01-03 23:16:11 -0700

    fix socket urls

--- app/dashboard/reservation/_components/reservation-listing-page.tsx
+++ app/dashboard/reservation/_components/reservation-listing-page.tsx
@@ -90,7 +90,6 @@ export default function ReservationListingPage() {
     fetchReservations();
     if (!isArchive) {
       const socketInstance = io(process.env.NEXT_PUBLIC_SOCKET_URL!, {
-        path: `${process.env.NEXT_PUBLIC_SOCKET_URL}/socket.io`,
         query: {
           organizationId: session.user.organizationId || ''
         },
